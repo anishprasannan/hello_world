c9d96a91fc0ec671b9b72647aa4fe896fc42152e
Anish Prasannan <anish.prasannan86@gmail.com>
2016-05-18 23:47:50 +1200

    HelloWorld:First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Anish Prasannan on 16/05/2016.
+ */
+public class HelloWorld {
+
+    private String helloWorld = "Hello World";
+
+    public HelloWorld(String input){
+        this.helloWorld = input;
+    }
+    public HelloWorld() {
+
+    }
+
+    public String getHelloWorld(){
+        return helloWorld;
+    }
+}
